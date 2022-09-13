118a0510ba829690f21ba9cc6779195cfd796180
Glacy <glacy.kelly3@gmail.com>
2022-09-12 22:22:33 -0300

    Finalizando lógica de foco

--- src/app/shared/directives/keyboard-manager/keyboard-managed-item.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-managed-item.directive.ts
@@ -1,13 +1,16 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Directive({
   selector: '[keyboardManagedItem]',
 })
 export class KeyBoardManagedItemDirective {
+  @Output() public focused = new EventEmitter<void>();
+
   constructor(private elementRef: ElementRef<HTMLElement>){}
 
   public focus() {
     this.elementRef.nativeElement.focus();
+    this.focused.emit();
   }
 
   public isFocused(): boolean {
